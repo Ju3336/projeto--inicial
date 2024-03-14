bac8913ae34e8509c0a5d05877009d0f21c48838
Ju3336 <juana.ferreira.ju@gmail.com>
2024-03-14 09:48:51 -0300

    mensagem contém nome

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar,false);
+var nome = document.getElementById ("nome");
+
 function alertar (event) {
-    alert("Voce clicou no botao!!!"+ event.status);
+    alert("Voce clicou no botao!!!"+ nome. value );
 }
