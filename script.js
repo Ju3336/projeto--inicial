9c44b1242c6150b8337aba6b69e8d4d8c1b56662
Ju3336 <juana.ferreira.ju@gmail.com>
2024-03-19 08:23:23 -0300

    Formulário

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar,false);
 var nome = document.getElementById ("nome");
 
+var saida= document.getElementById( "saida-de dados");
+var email = document .getElementById("email");
 function alertar (event) {
-    alert("Voce clicou no botao!!!"+ nome. value );
+    //alert("Voce clicou no botao!!!"+ nome. value );
+    saida.Innertext= "Nome" + nome.value+
+    var email = document .getElementById("email");
+    "/n Email:"+ email.value;
 }
